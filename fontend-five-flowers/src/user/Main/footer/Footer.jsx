ad9413475192a750ae03c7ce9f358379c5c72818
mtuanvu <143334278+mtuanvu@users.noreply.github.com>
2024-06-25 16:01:28 +0700

    footer

--- fontend-five-flowers/src/user/Main/footer/Footer.jsx
+++ fontend-five-flowers/src/user/Main/footer/Footer.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
-import "./footer.scss"
-const Footet = () => {
-  return (
-    <div className='footer-container'>Footet</div>
-  )
-}
+import React from 'react';
+import { FaTwitter, FaFacebookF, FaPinterestP, FaInstagram } from 'react-icons/fa';
+import './footer.scss';
 
-export default Footet
+
+const Footer = () => {
+  return (
+    <div className="subscription">
+      <div className="logo">
+        <img src="//bikex-club.myshopify.com/cdn/shop/files/logo_a64769da-d543-47d9-bcc5-2594dc90597f_250x_1_x58@2x.png?v=1614290807" alt="Bike Club Logo" />
+      </div>
+      <h2 className="subscribe-title">Subscribe Our Newsletter</h2>
+      <div className="subscribe-form">
+        <input type="email" placeholder="Your email address" />
+        <button>SIGN UP</button>
+      </div>
+      <div className="social-links">
+        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+          <FaTwitter />
+        </a>
+        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+          <FaFacebookF />
+        </a>
+        <a href="https://pinterest.com" target="_blank" rel="noopener noreferrer">
+          <FaPinterestP />
+        </a>
+        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+          <FaInstagram />
+        </a>
+      </div>
+      <div className="horizontal-line"></div>
+      <div className="footer-links">
+        <a href="/terms">Terms & Conditions</a>
+        <a href="/privacy">Privacy Policy</a>
+        <a href="/press">Press Release</a>
+        <a href="/contact">Contact</a>
+      </div>
+      <div className="copyright">
+        © 2024 Bikex Club (password: buddha) Design Themes
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
