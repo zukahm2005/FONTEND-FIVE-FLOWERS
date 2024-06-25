a4ee77fba2b669b249c06c666e024e4fc83b0d09
PhanQuan2308 <hpq833592@gmail.com>
2024-06-25 18:16:29 +0700

    quân push
    change color when active

--- fontend-five-flowers/src/user/Main/header/navBar/NavBar.jsx
+++ fontend-five-flowers/src/user/Main/header/navBar/NavBar.jsx
@@ -1,26 +1,29 @@
-import React from 'react'
+import { motion } from 'framer-motion';
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import "./navBar.scss";
 
-import { Link } from 'react-router-dom'
-import "./navBar.scss"
 const NavBar = () => {
+  const location = useLocation();
+  const activePath = location.pathname;
+
   return (
     <div className='nav-main'>
-        <div className='nav-home'>
-            <Link to="/home"><p>Home</p></Link>
+      {['home', 'shop', 'news', 'aboutUs'].map((item, index) => (
+        <div key={index} className={`nav-${item}`}>
+          <Link to={`/${item}`}>
+            <motion.p
+              initial={{ color: '#000' }}
+              animate={{ color: activePath === `/${item}` ? '#fa3e33' : '#000' }}
+              transition={{ duration: 0.5 }}
+            >
+              {item.toUpperCase()}
+            </motion.p>
+          </Link>
         </div>
-        <div className='nav-shop'>
-            <Link to="/shop"><p>Shop</p></Link>
-        </div>
-        <div className='nav-news'>
-            <Link to="/news"><p>News</p></Link>
-        </div>
-        <div className='nav-aboutUs'>
-            <Link to="/aboutUs"><p>About Us</p></Link>
-        </div>
-        
-        
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
