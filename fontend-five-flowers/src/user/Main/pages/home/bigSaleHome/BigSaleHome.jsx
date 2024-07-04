290fc1af7314be88bb2ed124a68fb8c7019d3217
PhanQuan2308 <hpq833592@gmail.com>
2024-07-04 12:53:38 +0700

    bigsale

--- fontend-five-flowers/src/user/Main/pages/home/bigSaleHome/BigSaleHome.jsx
+++ fontend-five-flowers/src/user/Main/pages/home/bigSaleHome/BigSaleHome.jsx
@@ -1,22 +1,24 @@
-import React from 'react';
-import './bigSaleHome.scss';
+import React from "react";
+import "./bigSaleHome.scss";
 
 const BigSaleHome = () => {
   return (
-    <div className="dt-sc-section-wrapper" style={{ backgroundImage: 'url(https://bikex-club.myshopify.com/cdn/shop/files/img-4.jpg?v=1614291618)'}}>
-      <div className="container"> 
-        <div className="row">
-          <div className="dt-sc-deal-banner-section">
-            <div className="dt-sc-deal-banner-content text-start">
-              <h4 className="dt-sc-main-title">Big <span>Sale</span></h4>
-              <h6 className="dt-sc-sub-title">NEW BIKE BY MANUFACTURES BIKO</h6>
-              <a href="/collections/hybrid-bikes" className="dt-sc-btn">BUY NOW</a>
-            </div> 
-          </div>           
+    <div className="big-sale-container">
+      <div className="info-big-sale-container">
+        <div className="name-big-sale">
+          <p>BIG SALE</p>
+        </div>
+        <div className="title-big-sale">
+          <p>NEW BIKE BY MANUFACTURES BIKO</p>
+        </div>
+        <div className="button-big-sale">
+          <button>
+            <p>BUY NOW</p>
+          </button>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default BigSaleHome;
