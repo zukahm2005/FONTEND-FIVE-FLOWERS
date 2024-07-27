1a486aa6a9c62555458375c1819e92dea4d83496
tuanphi1995 <145833059+tuanphi1995@users.noreply.github.com>
2024-07-27 22:09:49 +0700

    up

--- fontend-five-flowers/src/user/Main/header/components/iconContextCart/IconContextCart.jsx
+++ fontend-five-flowers/src/user/Main/header/components/iconContextCart/IconContextCart.jsx
@@ -1,10 +1,15 @@
 import { Badge } from 'antd';
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { IoCartOutline } from "react-icons/io5";
 import { CartContext } from '../cart/cartContext/CartProvider';
 
 const IconContextCart = () => {
   const { distinctProductCount } = useContext(CartContext);
+
+  // Kiểm tra giá trị distinctProductCount mỗi khi thay đổi
+  useEffect(() => {
+    console.log("distinctProductCount has changed:", distinctProductCount);
+  }, [distinctProductCount]);
 
   return (
     <div style={{ position: 'relative', display: 'inline-block' }}>
