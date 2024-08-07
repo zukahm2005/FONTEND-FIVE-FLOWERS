af957415417fe5b426ae8ec423bceaf82d0b95ff
Lương Tuấn Phi <hpq833592@gmail.com>
2024-08-07 23:01:42 +0700

    Update VideoHome.jsx

--- fontend-five-flowers/src/user/Main/pages/home/videoHome/VideoHome.jsx
+++ fontend-five-flowers/src/user/Main/pages/home/videoHome/VideoHome.jsx
@@ -52,8 +52,7 @@ const VideoHome = () => {
         </button>
         <div className="video-wrapper">
           <iframe
-          
-            src="https://www.youtube.com/embed/JJMPKgyCoSY"
+            src="https://www.youtube.com/embed/U9AnEaC1J8k"
             title="YouTube video player"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
